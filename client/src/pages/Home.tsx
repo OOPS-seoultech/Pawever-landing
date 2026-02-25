6680ad05494b78d64c40d90d06804d6702229cd7
Hyo <lovellehyo@gmail.com>
2026-02-25 14:50:44 +0900

    오류 수정

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -195,7 +195,7 @@ export default function Home() {
             <div className="flex flex-col items-center animate-fade-in">
               <div className="relative mb-6 rounded-3xl overflow-hidden shadow-2xl hover:shadow-3xl transition-all duration-300 hover:scale-105 bg-white">
                 <img
-                  src="public/memorial.png"
+                  src="/memorial.png"
                   alt="Pawever 별자리 추모관 화면"
                   className="w-full h-auto max-w-xs"
                 />
